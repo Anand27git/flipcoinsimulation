34fd3e062e13cea7a8733c10e1da4eeeadacc13c
Anand27git <anandkumar256k@gmail.com>
2021-05-09 09:29:48 +0530

    commit success

--- flipcoin.sh
+++ flipcoin.sh
@@ -13,3 +13,25 @@ else
 echo "tail is winner"
 fi
 done
+
+#counting
+headCount=0
+tailCount=0
+while [[ $headCount -le 20 && $tailcount -le 20 ]]
+do
+flip=$((RANDOM%2))
+if [[ flip -eq 0 ]]
+then
+headCount=$((headCount+1))
+else
+tailCount=$((tailCount+1))
+fi
+done
+if [[ $headCount -le 20 ]]
+then
+echo "Head wins"
+echo "tailCount was $tailCount"
+else
+echo "Tail wins"
+echo "Head Count was $headCount"
+fi
