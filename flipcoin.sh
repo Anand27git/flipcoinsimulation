e55cd6985fc09bbb6340d8a0dc3cbd9556e848b8
Anand27git <anandkumar256k@gmail.com>
2021-05-09 10:50:28 +0530

    commit success

--- flipcoin.sh
+++ flipcoin.sh
@@ -2,6 +2,7 @@
 
 echo "welcome to the flip coin simulation"
 
+
 for((i=0; i<10; i++))
 do
 toss=$((RANDOM%2))
@@ -34,4 +35,56 @@ echo "tailCount was $tailCount"
 else
 echo "Tail wins"
 echo "Head Count was $headCount"
+fi
+
+#uc--4
+declare -A coin
+
+	echo "${coin[@]}"
+read -p "enter you call 0(HEAD) and 1 (Tail):"choice
+i=0
+while [[ $((coin['Heads'])) -lt 21 || $((coin ['Tails'])) -lt 21 ]]
+do
+toss=$((RANDOM%2))
+if [[ $toss -eq 0 ]] ;
+ then
+	coin['Heads']=$((coin['Heads'] + 1))
+else
+        coin['Tails']=$((coin['Tails'] + 1))
+fi
+(( i++ ))
+done
+
+echo "Keys: ${!coin[@]}"
+echo "Values: ${coin[@]}"
+
+if [[ $((coin['Heads'])) -eq $((coin['Tails'])) ]]
+then
+echo "Its a Draw, Lets toss some more times."
+i=0
+while [[ $(( $((coin['Heads']))-$((coin['Tails'])) )) -lt 2 && $(( $((coin['Tails']))-$((coin['Heads'])) )) -lt 2 ]];
+do 
+toss=$((RANDOM%2))
+	if [[ $toss -eq 0 ]];
+then
+coin['Heads']=$((coin['Heads'] + 1 ))
+else
+coin['Tails']=$((coin['Tails'] + 1 ))
+fi
+((i++))
+done
+if [[$((coin['Heads'])) -gt $((coin['Tails'])) && choice -eq 0 ]]
+then
+	echo"Player1 won by $(( $((coin['Heads'}))-$((coin['Tails'])))) tosses."
+elif [[ $((coin['Heads'])) -gt $((coin['Tails'])) && $choice -eq 0 ]]
+then
+	echo "Player1 won by $(( $((coin['Heads']))-$((coin['Tails'])))) tosses."
+elif [[ $((coin['Heads'])) -gt $((coin['Tails'])) && $choice -eq 1 ]]
+then
+echo "Player1 won by $(($((coin['Heads'}))-$((coin['Tails'])))) tosses."
+elif [[ $((coin['Tails'])) -gt $((coin['Heads'])) && $choice -eq 0 ]]
+then
+echo "player2won by $(($((coin['Tails']))-$((coin['Heads'])))) tosses."
+else
+echo "Player2won by $(($((coin['Tails']))-$((coin['Heads'])))) tosses."
 fi
